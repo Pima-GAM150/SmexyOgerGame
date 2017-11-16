9c8c8d409962268a17f10403f61b6af008280a69
Darmek <adunne1@mail.pima.edu>
2017-11-16 00:54:04 -0700

    changed UITimer Script a bunch. Now has a "starttext" that freezes game till the space bar is pressed. also added code to switch between players after timer runs down.

--- Assets/UITimer.cs
+++ Assets/UITimer.cs
@@ -5,52 +5,79 @@ using UnityEngine.UI;
 
 public class UITimer : MonoBehaviour {
 
-    public int timeLeft = 10;
-    public int currentTime;
+    public float startTime = 10;
+    private float TimeLeft;    
     public Text timerCountDown;
     public Slider timeBarP1;
     public Slider timeBarP2;
     public Transform turnOverUI;
+    public Text startText;
+    public GameObject player1UI;
+    public GameObject player2UI;
 
     // Use this for initialization
     void Start()
     {
-        currentTime = timeLeft;
-        StartCoroutine("TurnOver");
-
+        // setting timer
+        TimeLeft = startTime;
+        
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-        timerCountDown.text = ("Time Left: " + timeLeft);
-
-        if (timeLeft <= 0)
+    {//setting up a start text to begin the game that way timer does not start right away
+        if (startText.gameObject.activeInHierarchy == true)
         {
-            StopCoroutine("TurnOver");
-            timerCountDown.text = "Out of Time! Switch Oger";
-            if (turnOverUI.gameObject.activeInHierarchy == false)
+            Time.timeScale = 0;
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                startText.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+        }
+        
+        timerCountDown.text = ("Time Left: " + Mathf.Round (TimeLeft));
+        TimeLeft -= Time.deltaTime;
+
+        if (TimeLeft <= 0)
+        {
+                        
+            if (turnOverUI.gameObject.activeInHierarchy == false)//once time = 0 this pauses game
             {
                 turnOverUI.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                                                
             }
-            
-        }
-
-        timeBarP1.maxValue = currentTime;
-            timeBarP1.value = timeLeft;
-            timeBarP2.maxValue = currentTime;
-            timeBarP2.value = timeLeft;
-
-        }
-
-        IEnumerator TurnOver()
-        {
-            while (true)
+            if (Input.GetKeyDown(KeyCode.Space))//this starts next round and restarts the time
             {
-                yield return new WaitForSeconds(1);
-                timeLeft--;
+                turnOverUI.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                TimeLeft = startTime;
+
+                // switches timer bar back forth between player turns
+
+                if (player2UI.activeInHierarchy == true)
+                {
+                    player2UI.SetActive(false);
+                    player1UI.SetActive(true);
+                }
+                else
+                if (player1UI.activeInHierarchy == true)
+                {
+                    player1UI.SetActive(false);
+                    player2UI.SetActive(true);
+                }               
+
             }
-        }
-    }
+
+        }           
+        //this sets the timer bars to match the time count down
+            timeBarP1.maxValue = startTime;
+            timeBarP1.value = TimeLeft;
+            timeBarP2.maxValue = startTime;
+            timeBarP2.value = TimeLeft;
+
+        }  
+    
+}
 
