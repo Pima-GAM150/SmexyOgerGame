b2ef4ea3261ef4fbbec3d2ae23ee16ad5c837b5d
DESKTOP-1SV3R20\jenma <flirn2@gmail.com>
2017-11-15 22:22:04 -0700

    Got text script working for, applicable for anything that ogres can interact with. Also got camera set up on the ogre prefab, created a new prefab for it

--- Assets/FontBehavior.cs
+++ Assets/FontBehavior.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class FontBehavior : MonoBehaviour {
 
+    public Transform playerPos;
     Vector3 scale = new Vector3(0.001f,0.001f,0.001f);
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Transform>().localScale += scale;
-        if (GetComponent<Transform>().localScale.x > 1.1)
+        transform.GetChild(0).transform.localScale += scale;
+        transform.GetChild(0).transform.LookAt(-playerPos.position);
+        if (transform.GetChild(0).transform.localScale.x > 1.1)
             scale *= -1;
-        if (GetComponent<Transform>().localScale.x < .9)
+        if (transform.GetChild(0).transform.localScale.x < .9)
             scale *= -1;
-
 	}
 
-    void Start () {
-        gameObject.SetActive(false);
-    }
-
-    void EnableDisable(bool input) {
-        gameObject.SetActive(input);
+    public void EnableDisable(bool input) {
+        transform.GetChild(0).gameObject.SetActive(input);
     }
 }
---
+++ Assets/OgreDistance.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OgreDistance : MonoBehaviour {
+
+    //This script is used to tell the human or any other objects that the ogre gets close to
+    //to give the ogre permission to interact with them and then also make text appear over
+    //the interactable object.
+
+    public GameObject human;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(human.transform.position,gameObject.transform.position) < 5) {
+            human.GetComponentInChildren<FontBehavior>().EnableDisable(true);
+            //give permission to controller to pick up human
+        }
+        else human.GetComponentInChildren<FontBehavior>().EnableDisable(false);
+    }
+}
