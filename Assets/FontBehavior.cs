f3906ab03986e0c22492201893ea2107b439ba5e
DESKTOP-1SV3R20\jenma <flirn2@gmail.com>
2017-11-15 17:46:28 -0700

    Created a scene, created a script for the text, and created a prefab

---
+++ Assets/FontBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FontBehavior : MonoBehaviour {
+
+    Vector3 scale = new Vector3(0.001f,0.001f,0.001f);
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Transform>().localScale += scale;
+        if (GetComponent<Transform>().localScale.x > 1.1)
+            scale *= -1;
+        if (GetComponent<Transform>().localScale.x < .9)
+            scale *= -1;
+
+	}
+
+    void Start () {
+        gameObject.SetActive(false);
+    }
+
+    void EnableDisable(bool input) {
+        gameObject.SetActive(input);
+    }
+}
