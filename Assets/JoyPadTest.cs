4da75c53e0b8d9276e28f9ad97aa1ec786447ec5
DESKTOP-1SV3R20\jenma <flirn2@gmail.com>
2017-11-16 03:51:45 -0700

    Fixxed the inputManager in project settings so the right stick works, also created my own version of the controller for an example

---
+++ Assets/JoyPadTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyPadTest : MonoBehaviour {
+
+    public GameObject P1;
+	
+	// Update is called once per frame
+	void Update () {
+        //this line gathers the direction the player wants to go
+        Vector3 dirToMove = new Vector3(-Input.GetAxis("Joy1_LeftXAxis") * .25f, 0.0f, Input.GetAxis("Joy1_LeftYAxis") * .25f);
+        //Vector3 dirToMove = new Vector3(-rightJoystickMovementX, 0.0f, rightJoystickMovementY);
+        //gameObject.transform.Translate(dirToMove);
+        //gameObject.transform.position += dirToMove;
+
+        //P1.transform.Rotate(0.0f, rightJoystickMovementX, 0.0f);
+        //gameObject.transform.Rotate(0.0f, rightJoystickMovementX, 0.0f);
+
+        //with that direction it account for which direction the player is facing and moves accordingly
+        P1.transform.Rotate(0.0f, Input.GetAxis("Joy1_RightXAxis"), 0.0f);
+        gameObject.transform.position += P1.transform.TransformDirection(dirToMove);
+        
+
+    }
+}
