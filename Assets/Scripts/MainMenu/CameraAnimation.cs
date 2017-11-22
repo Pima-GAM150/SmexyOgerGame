30e3a7a2f749414f2e4c2a8940ad7fcc2bc6d9b5
Morgan Ferguson <mfergu>
2017-11-22 12:26:32 -0700

    Camera Animations and UI Placeholder

---
+++ Assets/Scripts/MainMenu/CameraAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraAnimation : MonoBehaviour {
+
+	public Animator animator;
+
+	public void ClickOnSettings(){
+	
+		animator.SetTrigger ("SettingsClick");
+	}
+
+	public void ClickOnCredits(){
+
+		print ("ClickedOnCredits");
+	}
+
+	public void ClickOnPlay(){
+
+		print ("ClickedOnPlay");
+	}
+}
