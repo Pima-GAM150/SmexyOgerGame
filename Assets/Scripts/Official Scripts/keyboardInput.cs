d18dca79f9da4e1f9fc7b3e00910f8746adc0b63
LAPTOP-RR70KLG2\ezio9 <tpreble@mail.pima.edu>
2017-11-26 22:20:05 -0700

    Added scripts for each input mode (Controller + Keyboard, Keyboard, Controller + Controller, and Single Controller).  Added the controllerController script which will allow input methods to be enabled/disabled by the use of buttons.  Will adding in rotation to the control scripts shortly.

---
+++ Assets/Scripts/Official Scripts/keyboardInput.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyboardInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        CheckWASDMovement();
+        CheckArrowMovement();
+
+    }
+
+    private void CheckWASDMovement()
+    {
+        //Standard Keyboard Input
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.Translate(new Vector3(1, 0, 0) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.Translate(new Vector3(-1, 0, 0) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, 1) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, -1) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+    }
+
+    private void CheckArrowMovement()
+    {
+        //Arrow Key Keyboard Input
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.Translate(new Vector3(1, 0, 0) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.Translate(new Vector3(-1, 0, 0) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, 1) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.Translate(new Vector3(0, 0, -1) * 6 * Time.deltaTime);
+            //animator.SetBool("isMoving", true);  <- This will be used for the state machine later, so I'll just leave it here.
+        }
+    }
+}
