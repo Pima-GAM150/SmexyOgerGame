b5269ec5398f5feab404e880258188a9c45985d6
Morgan Ferguson <mfergu>
2017-11-28 14:25:06 -0700

    Added outline materials for objects.

---
+++ Assets/Scripts/Outline/ShaderToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShaderToggle : MonoBehaviour {
+
+	public Material defaultMaterial;
+	public Material outlineMaterial;
+	public Renderer objectRenderer;
+
+	// Use this for initialization
+	void Start () {	
+	
+		objectRenderer.material = defaultMaterial;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.S)) {
+			objectRenderer.material = outlineMaterial;
+		}		
+			
+	}
+}
