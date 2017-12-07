fb2b9f07515599248849ab917941bf306e55124d
DESKTOP-8JCA1RT\Quintin <qriehle@mail.pima.edu>
2017-12-07 00:54:51 -0700

    Player turn timer set up with slider working and linked play button to main scene so when you click it in play mode it brings you to the game

--- Assets/Scripts/MenuSelect.cs
+++ Assets/Scripts/MenuSelect.cs
@@ -33,8 +33,8 @@ public class MenuSelect : MonoBehaviour {
     }
     
     //use to load start game
-    //public void LoadOn()
-    //{
-      //  Application.LoadLevel(put name of scene here);
-    //}
+    public void LoadOn()
+    {
+       Application.LoadLevel (1);
+     }
 }
