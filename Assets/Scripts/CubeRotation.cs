337e77e83e0441bdfe77d641ccf3adb3de0fd326
Morgan Ferguson <mfergu>
2017-12-13 12:06:53 -0700

    Added action cube rotation.

---
+++ Assets/Scripts/CubeRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotation : MonoBehaviour {
+
+	public GameObject cube;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.I)) {
+			transform.Rotate (new Vector3 (0, 90, 0));
+		}
+	}
+}
